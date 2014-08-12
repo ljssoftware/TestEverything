da0839f3ff258a47f67c36073d91b1b46e023e56
lijinsheng <ljs_software@163.com>
2014-08-12 09:45:35 +0800

    完善zookeeper 测试用例
    完善zookeeper 测试用例

--- source code/java-zookeeper/src/main/java/com/testeverything/zookeeper/ZookeeperAdminTest.java
+++ source code/java-zookeeper/src/main/java/com/testeverything/zookeeper/ZookeeperAdminTest.java
@@ -7,9 +7,7 @@ import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.data.Stat;
 import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,7 +17,7 @@ import java.util.List;
  */
 public class ZookeeperAdminTest {
     private static final String IP = "192.168.215.128:2181,192.168.215.129:2181"; //zookeeper的IP地址， 所有的服务器信息
-    private static final int CONNECTION_TIMEOUT = 5000; // 连接超时时间, 单位是毫秒
+    private static final int SESSION_TIME_OUT = 500000; // 连接超时时间, 单位是毫秒
 
     //    管理的用户名 密码的相关配置
     private static final String ADMIN_USER_NAME = "admin_username"; // 管理者的用户名
@@ -29,37 +27,46 @@ public class ZookeeperAdminTest {
     private static final String GUEST_USER_NAME = "guest_username"; //访问者的用户名
     private static final String GUEST_PASSWORD = "guest_password"; //访问这的密码
 
+    private static final String PARENT_NODE = "/testRootPathDuplicate";
+
 
     public static void main(String[] args) {
         ZooKeeper zooKeeper = null;
         try {
             //连接zookeeper的集群
-            zooKeeper = new ZooKeeper(IP, CONNECTION_TIMEOUT, new MyWatcher());
+            zooKeeper = new ZooKeeper(IP, SESSION_TIME_OUT, new AdminWatcher());
+            zooKeeper.addAuthInfo("digest", (ADMIN_USER_NAME + ":" + ADMIN_PASSWORD).getBytes());
 
-            //ACL 相关信息
+            //根节点 如果 不存在则创建根节点
             Id adminId = new Id("digest", DigestAuthenticationProvider.generateDigest(ADMIN_USER_NAME + ":" + ADMIN_PASSWORD));
             ACL adminAcl = new ACL(ZooDefs.Perms.ALL, adminId);
 
+            String realPath;
+
+            //如果项目的根节点不存在，创建自己的根节点
+            Stat stat = null;
+            stat = zooKeeper.exists(PARENT_NODE, true);
+            if (stat == null) {
+                List<ACL> aclList = Collections.singletonList(adminAcl);
+                realPath = zooKeeper.create(PARENT_NODE, "root node".getBytes(), aclList, CreateMode.PERSISTENT);
+                LogHelper.ZOOKEEPER_LOGGER.info("ZookeeperAdminTest root node data:" + new String(zooKeeper.getData(realPath, false, null)) + ";real Path:" + realPath);
+            }
+            int version = stat.getVersion();
+            zooKeeper.setData(PARENT_NODE, "root node change".getBytes(), version);
+            LogHelper.ZOOKEEPER_LOGGER.info("ZookeeperAdminTest root node data:" + new String(zooKeeper.getData(PARENT_NODE, false, null)));
+
+            //ACL 相关信息
             Id guestId = new Id("digest", DigestAuthenticationProvider.generateDigest(GUEST_USER_NAME + ":" + GUEST_PASSWORD));
             ACL guestAcl = new ACL(ZooDefs.Perms.READ, guestId);
 
 
-            // 创建第一层节点， 并且 设置数据
-            String parentNode = "/testRootPath";
-            String rootData = "root Node";
-
-            List<ACL> aclList = Collections.singletonList(adminAcl);
-            String realPath = zooKeeper.create(parentNode, rootData.getBytes(), aclList, CreateMode.PERSISTENT_SEQUENTIAL);
-            zooKeeper.addAuthInfo("digest", (ADMIN_USER_NAME + ":" + ADMIN_PASSWORD).getBytes());
-            LogHelper.ZOOKEEPER_LOGGER.info("ZookeeperAdminTest root node data:" + new String(zooKeeper.getData(parentNode, false, null)) + ";real Path:" + realPath);
-
             //创建子节点 设置数据， 更新数据  并删除节点
-            String dataNode = parentNode + "/data";
+            String dataNode = PARENT_NODE + "/data";
             String dataData = "data Node";
             List<ACL> aclList2 = new ArrayList<ACL>(2);
             aclList2.add(adminAcl);
             aclList2.add(guestAcl);
-            realPath = zooKeeper.create(dataNode, dataData.getBytes(), aclList2, CreateMode.PERSISTENT);
+            realPath = zooKeeper.create(dataNode, dataData.getBytes(), aclList2, CreateMode.PERSISTENT_SEQUENTIAL);
             LogHelper.ZOOKEEPER_LOGGER.info("ZookeeperAdminTest data node data:" + new String(zooKeeper.getData(dataNode, false, null)) + ";real Path:" + realPath);
         } catch (Exception e) {
             LogHelper.ZOOKEEPER_LOGGER.error("ZookeeperAdminTest 操作zookeeper 失败", e);
